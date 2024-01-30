1425dbbadb9c80342b7869200220c5dbedd5a8d5
suckmyleg <juanito-146@hotmail.es>
2024-01-30 20:40:50 +0100

    pepe

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,4 @@
 
 # Aqui indicamos que se cargaran los ficheros "biblioteca_comic.py" y "biblioteca_comic_categoria"
 # Si creamos mas modelos, deben importarse en este fichero
-from . import biblioteca_comic
-from . import biblioteca_comic_categoria
 from . import saga
